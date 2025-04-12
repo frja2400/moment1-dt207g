a9e4e15baf4e493c15aee52ca905e301379a2b50
Frida Jansson <frja2400@student.miun.se>
2025-04-12 12:32:14 +0200

    Kodade app.post för att se att mina kurser adderades till databasen från formuläret.

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const { Client } = require("pg");
 const express = require("express");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(express.urlencoded( { extended: true })); //Aktivera formulärdata
+app.use(express.urlencoded({ extended: true })); //Aktivera formulärdata
 
 //Anslut till databasen
-const client = new Client ({
+const client = new Client({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
@@ -20,7 +21,7 @@ const client = new Client ({
 });
 
 client.connect((err) => {
-    if(err) {
+    if (err) {
         console.log("Fel vid anslutning: " + err);
     } else {
         console.log("Ansluten till databasen");
@@ -28,19 +29,32 @@ client.connect((err) => {
 });
 
 //Routing
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.render("index");
 });
 
-app.get("/courses", (req, res)=> {
+app.get("/courses", async (req, res) => {
     res.render("courses");
 });
 
-app.get("/about", (req, res)=> {
+app.post("/courses", async (req, res) => {
+    const coursecode = req.body.coursecode;
+    const coursename = req.body.coursename;
+    const syllabus = req.body.syllabus;
+    const progression = req.body.progression;
+
+    let error = "";
+
+    await client.query("INSERT INTO courses (coursecode, coursename, syllabus, progression) VALUES($1, $2, $3, $4)", [coursecode, coursename, syllabus, progression]);
+    res.redirect("/courses");
+});
+
+app.get("/about", (req, res) => {
     res.render("about");
 });
 
 //starta servern
-app.listen(process.env.PORT, ()=> {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("Servern startad på port: " + process.env.PORT);
 });
